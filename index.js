0def308f81b1078fa43cfe341b081ca94e26b628
LivisonVinicius <livisongc@gmail.com>
2022-07-04 05:14:56 -0300

    records, deposit and withdraw implemented

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import joi from "joi";
 import { MongoClient, ObjectId } from "mongodb";
-import bcrypt from "bcrypt"
-import { v4 as uuid } from 'uuid';
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+import dayjs from "dayjs";
 
 dotenv.config();
 
@@ -25,6 +26,11 @@ const userSchema = joi.object({
   pwd: joi.string().required(),
 });
 
+const moneySchema = joi.object({
+  value: joi.number().required(),
+  description: joi.string().required(),
+});
+
 app.post("/register", async (req, res) => {
   const validation = userSchema.validate(req.body, {
     abortEarly: true,
@@ -33,7 +39,7 @@ app.post("/register", async (req, res) => {
     .collection("users")
     .findOne({ user: req.body.user });
 
-    const existingEmail = await db
+  const existingEmail = await db
     .collection("users")
     .findOne({ email: req.body.email });
 
@@ -46,14 +52,12 @@ app.post("/register", async (req, res) => {
     return;
   }
   try {
-    const encryptedPwd = bcrypt.hashSync(req.body.pwd, 10) ;
-    await db
-      .collection("users")
-      .insertOne({
-        user: req.body.user,
-        email: req.body.email,
-        pwd: encryptedPwd,
-      });
+    const encryptedPwd = bcrypt.hashSync(req.body.pwd, 10);
+    await db.collection("users").insertOne({
+      user: req.body.user,
+      email: req.body.email,
+      pwd: encryptedPwd,
+    });
     res.sendStatus(201);
     return;
   } catch (error) {
@@ -63,37 +67,164 @@ app.post("/register", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-    const email = req.body.email;
-    const pwd = req.body.pwd;
-    const token = uuid();
-    
-    try {
-      const tryLogin = await db.collection("users").findOne({email: email });
-      const vrfLogin = await db.collection("onlineUsers").findOne({id:tryLogin._id})
-      console.log(token)
-      if (!tryLogin) {
-        res.sendStatus(404);
-      }
-      if (!bcrypt.compareSync(pwd,tryLogin.pwd ) ) {
-        res.sendStatus(400);
-        return;
-      }
-      if (vrfLogin){
-        await db.collection("onlineUsers").updateOne({userID:tryLogin._id},{$set:{acessToken:token}})
-      } 
-      if (!vrfLogin){
-        await db.collection("onlineUsers").insertOne({ userID:tryLogin._id, acessToken:token})
-      }
-      const respToken = await db.collection("onlineUsers").findOne({userID:tryLogin._id})
-      console.log(respToken)
-      const respUser = tryLogin.user
-      const respObj = {userName:respUser , acessToken: respToken.acessToken}
-      res.send(respObj).status(200);
-      return
-    }catch (error) {
-      res.sendStatus(500);
+  const email = req.body.email;
+  const pwd = req.body.pwd;
+  const token = uuid();
+
+  try {
+    const tryLogin = await db.collection("users").findOne({ email: email });
+    if (!tryLogin) {
+      res.sendStatus(404);
+    }
+    if (!bcrypt.compareSync(pwd, tryLogin.pwd)) {
+      res.sendStatus(400);
       return;
     }
+    const vrfLogin = await db
+      .collection("onlineUsers")
+      .findOne({ userID: tryLogin._id });
+    if (vrfLogin) {
+      await db
+        .collection("onlineUsers")
+        .updateOne({ userID: tryLogin._id }, { $set: { accessToken: token } });
+      console.log("foi editado");
+    }
+    if (!vrfLogin) {
+      await db
+        .collection("onlineUsers")
+        .insertOne({ userID: tryLogin._id, accessToken: token });
+      console.log("foi criado");
+    }
+    const respToken = await db
+      .collection("onlineUsers")
+      .findOne({ userID: tryLogin._id });
+    const respUser = tryLogin.user;
+    const respObj = { userName: respUser, accessToken: respToken.accessToken };
+    res.send(respObj).status(200);
+    return;
+  } catch (error) {
+    res.sendStatus(500);
+    return;
+  }
+});
+
+app.post("/deposit", async (req, res) => {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  console.log(token);
+
+  if (!token) {
+    res.sendStatus(402);
+    return;
+  }
+
+  const session = await db
+    .collection("onlineUsers")
+    .findOne({ accessToken: token });
+
+  if (!session) {
+    return res.sendStatus(401);
+  }
+
+  const validation = moneySchema.validate(req.body, {
+    abortEarly: true,
   });
+
+  if (validation.error) {
+    res.sendStatus(422);
+    return;
+  }
+
+  try {
+    await db.collection("transactions").insertOne({
+      userID: session.userID,
+      value: req.body.value,
+      description: req.body.description,
+      type: "deposit",
+      date: dayjs().format("DD/MM"),
+    });
+    res.sendStatus(200);
+    return;
+  } catch {
+    res.sendStatus(500);
+    return;
+  }
+});
+
+app.post("/withdraw", async (req, res) => {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) {
+    res.sendStatus(401);
+    return;
+  }
+
+  const session = await db
+    .collection("onlineUsers")
+    .findOne({ accessToken: token });
+
+  if (!session) {
+    return res.sendStatus(401);
+  }
+
+  const validation = moneySchema.validate(req.body, {
+    abortEarly: true,
+  });
+
+  if (validation.error) {
+    res.sendStatus(422);
+    return;
+  }
+
+  try {
+    await db.collection("transactions").insertOne({
+      userID: session.userID,
+      value: req.body.value,
+      description: req.body.description,
+      type: "withdraw",
+      date: dayjs().format("DD/MM"),
+    });
+    res.sendStatus(200);
+    return;
+  } catch {
+    res.sendStatus(500);
+    return;
+  }
+});
+
+app.get("/records", async (req, res) => {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) {
+    res.sendStatus(402);
+    return;
+  }
+
+  const session = await db
+    .collection("onlineUsers")
+    .findOne({ accessToken: token });
+
+  if (!session) {
+    return res.sendStatus(401);
+  }
+
+  try {
+    const transactionList = await db
+      .collection("transactions")
+      .find({ userID: session.userID })
+      .toArray();
+    transactionList.forEach((element) => {
+      delete element._id;
+    });
+    res.send(transactionList);
+    return;
+  } catch (error) {
+    res.sendStatus(500);
+    return;
+  }
+});
 
 app.listen(5000);
